6abcd940c80c7f0e87e6acaa2fe7a36dde955b6f
Sehajrattan Singh Dhillon <s.dhillon@mail.utoronto.ca>
2023-12-05 21:17:13 +0000

    CluesFeature and VisualizingRooms tests.

--- assignment2/BasicAdventureTest.java
+++ assignment2/BasicAdventureTest.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,41 +11,13 @@ import views.time;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BasicAdventureTest {
-    @Test
-    void getCommandsTest() throws IOException {
-        AdventureGame game = new AdventureGame("TinyGame");
-        String commands = game.player.getCurrentRoom().getCommands();
-        String[] lst = commands.split(",");
-        String[] lst2 = {"DOWN", "NORTH", "IN", "WEST", "UP", "SOUTH"};
-        if (lst.length == lst2.length) {
-            for (String element : lst2) {
-                assertEquals(helper(lst, element), true);
-            }
-        } else{
-            // Returns false
-            assertEquals(1, 2);
-        }
-    }
-    private static boolean helper(String[] lst, String word) {
-        for (String element : lst) {
-            if (element.equalsIgnoreCase(word)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Test
-    void getObjectString() throws IOException {
-        AdventureGame game = new AdventureGame("TinyGame");
-        String objects = game.player.getCurrentRoom().getObjectString();
-        assertEquals("a water bird", objects);
-    }
 
     //test for 1.1
     @Test
     void VisualizingRoom() throws IOException {
-        //
+        AdventureGame game = new AdventureGame("TinyGame");
+        int numOfRooms = game.getRooms().size();
+        assertEquals(numOfRooms, 12);
     }
 
     //test for 1.2
@@ -72,8 +43,14 @@ public class BasicAdventureTest {
     //test for 2.1
     @Test
     void CluesFeature() throws IOException {
-        //
+        // Test if the clues are in the correct rooms
+        AdventureGame game = new AdventureGame("TinyGame");
+        assertEquals(game.getRooms().get(1).cluesInRoom.get(0).getDescription(), "A small strand of soft chestnut coloured hair is on the ground, a couple feet away from where the body was."); // Room 1
+        assertEquals(game.getRooms().get(8).cluesInRoom.get(0).getDescription(), "A paper with the title \"DIVORCE\" are on the kitchen counter-top."); // Room 8
+        assertEquals(game.getRooms().get(4).cluesInRoom.get(0).getDescription(), "A personal diary with an unknown author is found on the nightstand expressing the frustrations towards her husband."); // Room 4
+        assertEquals(game.getRooms().get(5).cluesInRoom.get(0).getDescription(), "A pair of destroyed wedding rings is on the bathroom counter."); // Room 5
     }
+
 
     //test for 2.2
     @Test
@@ -96,7 +73,7 @@ public class BasicAdventureTest {
     //test for 3.1
     @Test
     void ColourInverted() throws IOException {
-        //
+        // can only be tested manually
     }
 
     //test for 3.2
