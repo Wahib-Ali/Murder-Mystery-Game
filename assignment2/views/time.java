498af63481cc6ae79b400b8e71fb37981e9aafd2
Sehajrattan Singh Dhillon <s.dhillon@mail.utoronto.ca>
2023-12-06 05:38:38 +0000

    Added Java doc for time class.

--- assignment2/views/time.java
+++ assignment2/views/time.java
@@ -3,15 +3,35 @@ public class time {
     int minute;
     int second;
 
+    /**
+     *
+     * @param minute The amount of minutes left in the countdown
+     * @param second The amount of seconds left in the countdown
+     */
     public time(int minute, int second) {
         this.minute = minute;
         this.second = second;
     }
 
+    /**
+     * getCurrentTime:
+     * ___________________________
+     *
+     * Getter method more the time remaining
+     *
+     * @return a string that represents the amount of time left to complete the game
+     */
+
     public String getCurrentTime(){
         return minute + ":" + second;
     }
 
+    /**
+     * oneSecondPassed
+     * ___________________________
+     *
+     * This method simulates one second passing in the countdown.
+     */
     public void oneSecondPassed(){
         second--;
         if(second == -1){
